63314c1987c5645bb9f49817f4161695d98507a7
monalisasingh87 <monalisa.singh87@gmail.com>
2025-04-01 23:41:40 +0000

    domain name generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let pronoun = ['the', 'our', 'your'];
+  let adj = ['great', 'happy','genius'];
+  let noun = ['dog', 'cat','cow'];
+  let extension = ['.com','.net','.org'];
+
+  let listElements = document.querySelector('.domain');
+  let listofItems = []
+
+  for (let i = 0; i < pronoun.length; ++i) { // outer loop
+    for (let j = 0; j < adj.length; ++j) { // inner loop
+      for (let k = 0; k < noun.length; ++k) { // inner loop
+        for (let l = 0; l < extension.length; ++l) { //inner loop
+
+        let temp = document.createElement('li');
+        temp.innerHTML = `${pronoun[i]}${adj[j]}${noun[k]}${extension[l]}`;
+        listElements.appendChild(temp);
+      }
+    }
+  }
+ }
+          
+ };
